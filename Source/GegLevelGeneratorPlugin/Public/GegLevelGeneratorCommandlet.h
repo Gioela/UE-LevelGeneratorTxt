df909b5ba92ea250886d0de6ff433435e7ed53e9
GRaiano <gioele.raiano@gmail.com>
2022-07-02 11:27:09 +0200

    Start Commandlet option

---
+++ Source/GegLevelGeneratorPlugin/Public/GegLevelGeneratorCommandlet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Commandlets/Commandlet.h"
+#include "GegLevelGeneratorCommandlet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEGLEVELGENERATORPLUGIN_API UGegLevelGeneratorCommandlet : public UCommandlet
+{
+	GENERATED_BODY()
+	
+};
